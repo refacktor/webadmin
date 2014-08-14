4b720efddcdc837717b4263a25894f3ca5fb7f73
Pcoder <purple.coder@yahoo.co.uk>
2014-08-14 17:14:07 +0200

    Update db.sql
    FIXED: ERROR 1071 (42000) at line 8: Specified key was too long; max key length is 767 bytes.

--- db.sql
+++ db.sql
@@ -4,9 +4,9 @@ use webadmin;
 
 CREATE TABLE IF NOT EXISTS `users` (
 	`uid` int(11) NOT NULL AUTO_INCREMENT,
-	`email` varchar(300) NOT NULL UNIQUE,
+	`email` varchar(255) NOT NULL UNIQUE,
 	`password` varchar(300) NOT NULL,
-	`activation` varchar(300) NOT NULL UNIQUE,
+	`activation` varchar(255) NOT NULL UNIQUE,
 	`status` enum('0','1') NOT NULL DEFAULT '0',
 	`owner_authorized` enum('0','1') NOT NULL DEFAULT '0',
 	`justification` varchar(300),
